fd45b11579975ec08d125638817af9afefd51d93
tonyngl777 <vincenttony777@gmail.com>
2023-10-28 10:12:35 +0530

    28/10

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+
+
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
@@ -7,11 +9,14 @@ import "../styles/login.css";
 
 export const Login = () => {
     const [email, setemail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState(""); 
-// eslint-disable-next-line
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState("");
+
     const [_, setCookies] = useCookies(["access_token"]);
     const navigate = useNavigate();
+
+    const [showPassword, setShowPassword] = useState(false);
+
 
     const onSubmit = async (event) => {
         event.preventDefault();
@@ -27,45 +32,68 @@ export const Login = () => {
             navigate("/");
         } catch (error) {
             console.error(error);
-            setError("Login failed. Please check your email and password."); // Set error message
-        }
-    };
+            setError("Login failed. Please check your email and password."); 
+        }};
+        const togglePassword = () => {
+            setShowPassword((showPassword) => !showPassword);
+          };
+       
 
     return (
         <div className="login container row col-12">
             <div className="form-container col-md-6 mt-5">
-                <h1 className="title">Welcome back!</h1>
-                <p className="subtitle">Please enter your details</p>
+                <h1 className="title">Sign In</h1>
                 <form onSubmit={onSubmit}>
                     <div className="mb-3">
+                       
                         <label htmlFor="email" className="form-label">Email</label>
+                       
                         <input
                             type="text"
                             className="form-input"
                             id="email"
+                            placeholder="me@example.com"
                             value={email}
                             onChange={(event) => setemail(event.target.value)}
                             required
+                           
                         />
                     </div>
+               
                     <div className="mb-3">
                         <label htmlFor="password" className="form-label">Password</label>
+                        <div className="password-input">
+                       
                         <input
-                            type="password"
+                            type={showPassword ? 'text' : 'password'}
                             className="form-input"
                             id="password"
+                            placeholder="password"
                             value={password}
                             onChange={(event) => setPassword(event.target.value)}
                             required
                         />
+                       
+                        <button
+                       type="button" className="show-password-icon"
+                        onClick={togglePassword}
+                        >
+                            {showPassword?'hide':'show'}
+                        </button>
+                        </div>
+                   
                     </div>
-
-                    <button type="submit" className="login-btn">Log In</button>
+                 
+                    <button type="submit" className="login-btn">LOG IN</button>
+                       
+                    </form>
                     {error && <p className="error-message">{error}</p>} {/* Display error message */}
-                </form>
                 <p className="register-text">Don't have an account?<Link className="register-link" to="/register">Sign Up</Link></p>
             </div>
-    <img id="logo" src={require("../assets/community.svg").default} alt="login" />
+
+           
         </div>
+   
+       
     );
 };
